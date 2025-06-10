b8314946708f62cebd3d3fe51807193d9d1a806d
ABDELHEDI AHMED <ahmed.abdelhedi01@esprit.tn>
2025-06-10 14:21:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,14 +10,11 @@ st.write(
   """Choose the fruits you want in your custom Smoothie!"""
 )
 
-
 name_on_order = st.text_input("Name on smoothie: ")
 st.write("The name on your smoothie will be ", name_on_order)
 
-
 cnx= st.connection("snowflake")
 session = cnx.session()
-
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
@@ -49,7 +46,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
 
-
-
+#new section to display smoothiefroot nutrition information 
+import requests 
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
     
 
