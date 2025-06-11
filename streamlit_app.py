0833bbaf07d2f2d82ad9126ec0ee4713880f4546
ABDELHEDI AHMED <ahmed.abdelhedi01@esprit.tn>
2025-06-11 12:26:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,12 +17,22 @@ st.write("The name on your smoothie will be ", name_on_order)
 cnx= st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 
-ingredients_list= st.multiselect('Choose up to 5 ingredients :'
-                                 ,my_dataframe 
-                                , max_selections= 5)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+
+# convert the snowpark dataframe to a pandas dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
+
+
+ingredients_list= st.multiselect(
+    'Choose up to 5 ingredients :'
+    , my_dataframe 
+    , max_selections= 5)
 
 if ingredients_list:
 
@@ -30,10 +40,16 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        sf_df= st.dataframe(data = smoothiefroot_response.json(), use_container_width= True)
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
+
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        fv_df = st.dataframe(data = fruityvice_response.json() , use_container_width= True)
+                             
+    
     my_insert_stmt = f"""INSERT INTO smoothies.public.orders (ingredients, NAME_ON_ORDER)
                          VALUES ('{ingredients_string}', '{name_on_order}')"""
 
@@ -45,6 +61,7 @@ if ingredients_list:
     if time_to_insert: 
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
+
 
 
 
